7ffafcbe17adc314f83df9c302111fa6261d3447
sbaker-dev <samuelbaker.researcher@gmail.com>
2022-09-15 16:03:19 +0100

    Hide and show elements for reply button

---
+++ Django/static/js/hideShow.js
@@ -0,0 +1,14 @@
+
+
+function hideElement(elementID){
+    let element = document.getElementById("BT-" + elementID)
+    element.classList.add('hidden')
+
+}
+
+function showElement(elementID){
+    let element = document.getElementById("H-" + elementID)
+    element.classList.remove('hidden')
+
+    hideElement(elementID)
+}
